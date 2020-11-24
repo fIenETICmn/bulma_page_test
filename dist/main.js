84aa2e05a9f90754f0243dd74bf97626e9bcc0d3
fIenETICmn <simarpreet@codefront.io>
2020-11-24 17:53:58 +0530

    refactor:IMprove code and bug

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,27 @@
+// notify-modal
+//const notifyicon = document.querySelector('#notify');
+//const modalBg = document.querySelector('.modal-background');
+//const modal = document.querySelector('.modal');
+//
+//notifyicon.addEventListener('click', () => {
+//  modal.classList.add('is-active');
+//})
+//
+//modalBg.addEventListener('click', () => {
+//  modal.classList.remove('is-active');
+//})
+
+// acct-setting-modal
 // modal
-const notifyicon = document.querySelector('#notify');
+const signupButton = document.querySelector('#acct_setting');
 const modalBg = document.querySelector('.modal-background');
 const modal = document.querySelector('.modal');
 
-notifyicon.addEventListener('click', () => {
+signupButton.addEventListener('click', () => {
   modal.classList.add('is-active');
 })
 
 modalBg.addEventListener('click', () => {
   modal.classList.remove('is-active');
 })
-
 
